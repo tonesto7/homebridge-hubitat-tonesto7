071719a4e4ae0f7235b4c553c0cd6cafdc182d33
imnotbob <Imnot_bob@hotmail.com>
2020-11-22 21:20:35 -0500

    Update homebridge-v2.groovy

--- apps/homebridge-v2.groovy
+++ apps/homebridge-v2.groovy
@@ -3,7 +3,7 @@
  *  App footer inspired from Hubitat Package Manager (Thanks @dman2306)
  *
  *  Copyright 2018, 2019, 2020 Anthony Santilli
- *  Contributions by @nh.schotfam
+ *  Contributions by @nh.schottfam
  */
 
 import groovy.transform.Field
